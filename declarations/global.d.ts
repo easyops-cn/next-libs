53131823b6e7db463fe96aba0d576f34065b08b3
stevewang <stevewang@easyops.cn>
2019-08-30 19:32:35 +0800

    chore(): refine deps

--- declarations/global.d.ts
+++ declarations/global.d.ts
@@ -8,40 +8,7 @@ declare module "*.css" {
   export default css;
 }
 
-declare module "*.less" {
-  const lessValue: string;
-  export default lessValue;
-}
-
-declare module "*.html" {
-  const html: string;
-  export default html;
-}
-
-declare module "*.md" {
-  const content: string;
-  export default content;
-}
-
-interface SvgrComponent
-  extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
-
-declare module "*.svg" {
-  const url: string;
-  export default url;
-  export const ReactComponent: SvgrComponent;
-}
-
-declare module "*.png" {
-  const url: string;
-  export default url;
-}
-
 declare namespace JSX {
-  interface IntrinsicElements {
-    slot: any;
-  }
-
   interface IntrinsicAttributes {
     ref?: React.LegacyRef<HTMLElement>;
     className?: string;
--- jest.config.js
+++ jest.config.js
@@ -24,15 +24,7 @@ module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "/template/"],
   moduleNameMapper: {
     "\\.module\\.css$": "identity-obj-proxy",
-    "\\.css$": "<rootDir>/__mocks__/styleMock.js",
-    "^.+\\.html$": "<rootDir>/__mocks__/htmlMock.js",
-    "\\.svg": "<rootDir>/__mocks__/svgrMock.js",
-    "\\.png": "<rootDir>/__mocks__/pngMock.js",
-    "^.+\\.md?$": "<rootDir>/__mocks__/markdownMock.js",
-    // Ref http://react-dnd.github.io/react-dnd/docs/testing#setup
-    "^dnd-core$": "dnd-core/dist/cjs",
-    "^react-dnd$": "react-dnd/dist/cjs",
-    "^react-dnd-html5-backend$": "react-dnd-html5-backend/dist/cjs"
+    "\\.css$": "<rootDir>/__mocks__/styleMock.js"
   },
   // Ref https://github.com/facebook/jest/issues/2070#issuecomment-431706685
   // Todo(steve): remove next line when issue fixed.
