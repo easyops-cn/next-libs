487d73e08bb4cd51f655265feabb6b5bfd8136fd
ice <iceliang@easyops.cn>
2019-09-04 17:11:11 +0800

    feat(): custom hook to confirm page leaving, refs ATM-434

---
+++ libs/custom-hook/src/useConfirmToLeave.ts
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { getHistory } from "@easyops/brick-kit";
+
+export const useConfirmToLeave = (
+  shouldBlockRef: React.MutableRefObject<boolean>,
+  message = "确定离开该页面?"
+): void => {
+  let historyUnblock: Function;
+
+  const beforeunload = (e: BeforeUnloadEvent) => {
+    if (shouldBlockRef.current) {
+      e.preventDefault();
+      e.returnValue = message;
+      return message;
+    } else {
+      delete e.returnValue;
+    }
+  };
+
+  useEffect(() => {
+    const history = getHistory();
+    historyUnblock = history.block(() => {
+      if (shouldBlockRef.current) {
+        return message;
+      }
+    });
+
+    window.addEventListener("beforeunload", beforeunload);
+
+    return () => {
+      historyUnblock();
+      window.removeEventListener("beforeunload", beforeunload);
+    };
+  }, []);
+};
