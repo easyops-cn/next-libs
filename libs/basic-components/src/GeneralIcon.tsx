d90e4d721505b459ec32fee12bdd28118dd67f2e
stevewang <stevewang@easyops.cn>
2019-09-24 19:00:20 +0800

    feat(): add easyops icons
    INFRA-434

--- libs/basic-components/src/GeneralIcon.tsx
+++ libs/basic-components/src/GeneralIcon.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Icon } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { MenuIcon } from "@easyops/brick-types";
+import { BrickIcon } from "@easyops/brick-icons";
 
 interface MenuIconProps {
   icon: MenuIcon;
@@ -18,6 +19,16 @@ export function GeneralIcon({ icon }: MenuIconProps): React.ReactElement {
 
   if (icon.lib === "fa") {
     return <Icon component={() => <FontAwesomeIcon icon={icon.icon} />} />;
+  }
+
+  if (icon.lib === "easyops") {
+    return (
+      <Icon
+        component={() => (
+          <BrickIcon icon={icon.icon} category={icon.category} />
+        )}
+      />
+    );
   }
 
   return null;
