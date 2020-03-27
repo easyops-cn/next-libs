29d65dfb86f581478831f6e6449cec333b9a05ca
stevewang <stevewang@easyops.cn>
2020-03-27 15:43:31 +0800

    feat(): drop unused code `Builder/*`
    INFRA-434

--- libs/storyboard-visualization/src/Builder/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./BuilderVisualization";
-export * from "./interfaces";
-export * from "./VisualBuilder";
--- libs/storyboard-visualization/src/index.ts
+++ libs/storyboard-visualization/src/index.ts
@@ -5,5 +5,4 @@ export * from "./storyboardToTree";
 export * from "./treeToStoryboard";
 export * from "./Visualization";
 export * from "./VisualStoryboard/VisualStoryboard";
-export * from "./Builder";
 export * from "./BuilderGraph";
