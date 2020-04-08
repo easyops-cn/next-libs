bee96966bfc1e06f9b40f09ba5ad7dd9851f3731
Lynette <lynetteli@easyops.cn>
2020-04-08 10:49:12 +0800

    feat(utils): getIconList
    refs NEXT_BUILDER-41

---
+++ libs/basic-components/src/utils/getIconList.spec.ts
@@ -0,0 +1,235 @@
+import { getIconList, GetIconListParams } from "./getIconList";
+
+jest.mock("@easyops/brick-icons", () => ({
+  iconsByCategory: {
+    app: ["all-cmdb", "brick-group"],
+    model: ["business", "cloud-host"]
+  }
+}));
+
+jest.mock("@easyops/fontawesome-library", () => ({
+  fab: {
+    fa500px: {
+      prefix: "fab",
+      iconName: "500px"
+    },
+    faAccusoft: {
+      prefix: "fab",
+      iconName: "accusoft"
+    }
+  },
+  fas: {
+    faAd: {
+      prefix: "fas",
+      iconName: "ad"
+    },
+    faAddressBook: {
+      prefix: "fas",
+      iconName: "address-book"
+    }
+  }
+}));
+
+jest.mock("@ant-design/icons/lib/manifest", () => ({
+  fill: ["account-book"],
+  outline: ["alert"],
+  twotone: ["alipay-circle"]
+}));
+
+describe("GetIconList", () => {
+  it.each<[GetIconListParams, any]>([
+    [
+      {},
+      {
+        list: [
+          {
+            title: "all-cmdb",
+            descriptionList: ["category: app", "icon: all-cmdb"],
+            icon: {
+              category: "app",
+              icon: "all-cmdb",
+              lib: "easyops"
+            }
+          },
+          {
+            title: "brick-group",
+            descriptionList: ["category: app", "icon: brick-group"],
+            icon: {
+              category: "app",
+              icon: "brick-group",
+              lib: "easyops"
+            }
+          },
+          {
+            title: "business",
+            descriptionList: ["category: model", "icon: business"],
+            icon: {
+              category: "model",
+              icon: "business",
+              lib: "easyops"
+            }
+          },
+          {
+            title: "cloud-host",
+            descriptionList: ["category: model", "icon: cloud-host"],
+            icon: {
+              category: "model",
+              icon: "cloud-host",
+              lib: "easyops"
+            }
+          }
+        ],
+        total: 4
+      }
+    ],
+    [
+      {
+        type: "easyops",
+        page: 1,
+        pageSize: 2
+      },
+      {
+        list: [
+          {
+            title: "all-cmdb",
+            descriptionList: ["category: app", "icon: all-cmdb"],
+            icon: {
+              category: "app",
+              icon: "all-cmdb",
+              lib: "easyops"
+            }
+          },
+          {
+            title: "brick-group",
+            descriptionList: ["category: app", "icon: brick-group"],
+            icon: {
+              category: "app",
+              icon: "brick-group",
+              lib: "easyops"
+            }
+          }
+        ],
+        total: 4
+      }
+    ],
+    [
+      {
+        q: "tttttt",
+        type: "easyops",
+        page: 1,
+        pageSize: 2
+      },
+      {
+        list: [],
+        total: 0
+      }
+    ],
+    [
+      {
+        q: "all-cmdb",
+        type: "easyops",
+        page: 1,
+        pageSize: 20
+      },
+      {
+        list: [
+          {
+            title: "all-cmdb",
+            descriptionList: ["category: app", "icon: all-cmdb"],
+            icon: {
+              category: "app",
+              icon: "all-cmdb",
+              lib: "easyops"
+            }
+          }
+        ],
+        total: 1
+      }
+    ],
+    [
+      {
+        type: "antd"
+      },
+      {
+        list: [
+          {
+            title: "account-book",
+            descriptionList: ["theme: filled", "icon: account-book"],
+            icon: {
+              icon: "account-book",
+              theme: "filled",
+              lib: "antd"
+            }
+          },
+          {
+            title: "alert",
+            descriptionList: ["theme: outlined", "icon: alert"],
+            icon: {
+              icon: "alert",
+              theme: "outlined",
+              lib: "antd"
+            }
+          },
+          {
+            title: "alipay-circle",
+            descriptionList: ["theme: twoTone", "icon: alipay-circle"],
+            icon: {
+              icon: "alipay-circle",
+              theme: "twoTone",
+              lib: "antd"
+            }
+          }
+        ],
+        total: 3
+      }
+    ],
+    [
+      {
+        type: "fa"
+      },
+      {
+        list: [
+          {
+            title: "500px",
+            descriptionList: ["prefix: fab", "icon: 500px"],
+            icon: {
+              icon: "500px",
+              lib: "fa",
+              prefix: "fab"
+            }
+          },
+          {
+            title: "accusoft",
+            descriptionList: ["prefix: fab", "icon: accusoft"],
+            icon: {
+              icon: "accusoft",
+              lib: "fa",
+              prefix: "fab"
+            }
+          },
+          {
+            title: "ad",
+            descriptionList: ["prefix: fas", "icon: ad"],
+            icon: {
+              icon: "ad",
+              lib: "fa",
+              prefix: "fas"
+            }
+          },
+          {
+            title: "address-book",
+            descriptionList: ["prefix: fas", "icon: address-book"],
+            icon: {
+              icon: "address-book",
+              lib: "fa",
+              prefix: "fas"
+            }
+          }
+        ],
+        total: 4
+      }
+    ]
+  ])("GetIconList(%j) should work", (params, result) => {
+    expect(getIconList(params)).toEqual(result);
+  });
+});
---
+++ libs/basic-components/src/utils/getIconList.ts
@@ -0,0 +1,94 @@
+import { iconsByCategory as easyopsByCategory } from "@easyops/brick-icons";
+import { fab, fas } from "@easyops/fontawesome-library";
+import antdByCategory from "@ant-design/icons/lib/manifest";
+
+export type IconType = "easyops" | "antd" | "fa";
+
+export interface GetIconListParams {
+  q?: string;
+  type?: IconType;
+  page?: number;
+  pageSize?: number;
+}
+
+export const antedThemeMap = {
+  outline: "outlined",
+  fill: "filled",
+  twotone: "twoTone"
+};
+
+export function getIconList(params: GetIconListParams) {
+  const { q = "", type = "easyops", page = 1, pageSize = 20 } = params;
+
+  const iconList: any[] = [];
+
+  if (type === "easyops") {
+    for (const [key, icons] of Object.entries(easyopsByCategory)) {
+      const list = icons.map(name => ({
+        title: name,
+        descriptionList: [`category: ${key}`, `icon: ${name}`],
+        icon: {
+          lib: type,
+          category: key,
+          icon: name
+        }
+      }));
+      iconList.push(...list);
+    }
+  } else if (type === "antd") {
+    for (const [key, icons] of Object.entries(antdByCategory)) {
+      const list = icons.map(name => ({
+        title: name,
+        descriptionList: [
+          `theme: ${antedThemeMap[key as keyof typeof antedThemeMap]}`,
+          `icon: ${name}`
+        ],
+        icon: {
+          lib: type,
+          icon: name,
+          theme: antedThemeMap[key as keyof typeof antedThemeMap]
+        }
+      }));
+      iconList.push(...list);
+    }
+  } else if (type === "fa") {
+    const faByCategory = { ...fab, ...fas };
+
+    for (const [, iconObj] of Object.entries(faByCategory)) {
+      iconList.push({
+        title: iconObj.iconName,
+        descriptionList: [
+          `prefix: ${iconObj.prefix}`,
+          `icon: ${iconObj.iconName}`
+        ],
+        icon: {
+          lib: "fa",
+          icon: iconObj.iconName,
+          prefix: iconObj.prefix
+        }
+      });
+    }
+  }
+
+  // 先过滤关键字
+  let filters = [];
+  if (q === "") {
+    filters = iconList;
+  } else {
+    const query = q.toLocaleLowerCase().trim();
+    filters = iconList.filter(
+      item =>
+        item.title.toLowerCase().includes(query) ||
+        item.descriptionList.find((i: string) =>
+          i.toLowerCase().includes(query)
+        )
+    );
+  }
+
+  // 再进行分页
+  const pageList = filters.slice((page - 1) * pageSize, page * pageSize);
+  return {
+    list: pageList,
+    total: filters.length
+  };
+}
