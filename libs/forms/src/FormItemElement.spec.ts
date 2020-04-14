86a8fd36ec6a5a67672e37e7c4af94597ec61a22
stevewang <stevewang@easyops.cn>
2020-04-14 11:16:02 +0800

    chore(): fix tests after upgraded jsdom@162

--- libs/forms/src/FormItemElement.spec.ts
+++ libs/forms/src/FormItemElement.spec.ts
@@ -1,5 +1,3 @@
-// Ref https://github.com/jsdom/jsdom/issues/1030
-import "document-register-element";
 import ReactDOM from "react-dom";
 import { FormItemElement } from "./FormItemElement";
 
@@ -15,26 +13,22 @@ const unmountComponentAtNode = jest
   .mockImplementation((() => null) as any);
 
 describe("form-item", () => {
-  it("should create a custom element", async () => {
+  it("should create a custom element", () => {
     const element = document.createElement("form-item") as TestFormItemElement;
-    const form = document.createElement("forms.general-form");
+    const form = document.createElement("forms.general-form") as any;
     form.isFormElement = true;
     document.body.appendChild(form);
 
     document.body.appendChild(element);
-    await jest.runAllTimers();
     expect(element.getFormElement()).toBe(null);
-
-    form.appendChild(element);
-    expect(element.getFormElement()).toBe(form);
     expect(element.style.display).toBe("block");
 
     form.layout = "inline";
-    await jest.runAllTimers();
+    form.appendChild(element);
+    expect(element.getFormElement()).toBe(form);
     expect(element.style.display).toBe("inline-block");
 
     form.removeChild(element);
-    await jest.runAllTimers();
     expect(unmountComponentAtNode).toBeCalled();
   });
 });
