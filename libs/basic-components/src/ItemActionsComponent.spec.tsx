6d9bef92f275b338691ee9b029bf0b303e2e2231
William Cai <williamcai@easyops.cn>
2020-09-09 11:08:02 +0800

    feat(basic-components): ItemActionsComponent add onVisibleChange property
    Refs BRICK_STORE-1666

--- libs/basic-components/src/ItemActionsComponent.spec.tsx
+++ libs/basic-components/src/ItemActionsComponent.spec.tsx
@@ -19,13 +19,17 @@ const actions = [
 
 describe("ItemActionsComponent", () => {
   it("should work", async () => {
+    const mockedOnVisibleChange = jest.fn();
     const wrapper = mount(
       <ItemActionsComponent
         item={{ id: 1, name: 1 }}
         filteredActions={actions}
+        onVisibleChange={mockedOnVisibleChange}
       />
     );
+    expect(mockedOnVisibleChange).not.toBeCalled();
     wrapper.find("Button").simulate("click");
+    expect(mockedOnVisibleChange).toBeCalled();
     wrapper.update();
     expect(wrapper.find("BrickAsComponent").length).toBe(2);
   });
