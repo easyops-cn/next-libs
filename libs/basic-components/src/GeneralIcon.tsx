b723ea297c908a62d74cceb7c96aea2de9ea396a
William Cai <williamcai@easyops.cn>
2020-09-17 11:06:40 +0800

    feat(GeneralIcon): add onClick property
    Refs BRICK_STORE-1666

--- libs/basic-components/src/GeneralIcon.tsx
+++ libs/basic-components/src/GeneralIcon.tsx
@@ -17,6 +17,7 @@ interface MenuIconProps {
   size?: number | "large" | "small" | "default";
   shape?: "circle" | "square";
   reverseBgColor?: boolean;
+  onClick?(event: React.MouseEvent<HTMLElement, MouseEvent>): void;
 }
 
 export enum Colors {
@@ -36,6 +37,7 @@ export function GeneralIcon({
   size,
   shape,
   reverseBgColor,
+  onClick,
 }: MenuIconProps): React.ReactElement {
   let iconNode = <></>;
   if (!icon) {
@@ -77,7 +79,14 @@ export function GeneralIcon({
   if (icon.lib === "antd") {
     const type =
       (icon as RefinedAntdIcon).icon ?? (icon as LegacyAntdIcon).type;
-    iconNode = <LegacyIcon type={type} theme={icon.theme} style={iconStyle} />;
+    iconNode = (
+      <LegacyIcon
+        type={type}
+        theme={icon.theme}
+        style={iconStyle}
+        onClick={onClick}
+      />
+    );
   }
 
   if (icon.lib === "fa") {
@@ -88,6 +97,7 @@ export function GeneralIcon({
       <Icon
         style={{ ...iconStyle, verticalAlign: 0 }}
         component={() => <FontAwesomeIcon icon={faIcon} />}
+        onClick={onClick}
       />
     );
   }
@@ -99,6 +109,7 @@ export function GeneralIcon({
         component={() => (
           <BrickIcon icon={icon.icon} category={icon.category} />
         )}
+        onClick={onClick}
       />
     );
   }
