0fd7477bdf755b7d1eda86bf9f08afdca572e38f
weareoutman <wangshenwei@qq.com>
2021-04-20 10:48:45 +0800

    test(): fix tests after migrated sdk

--- libs/cmdb-instances/src/data-providers/fetchCmdbInstanceDetail.spec.ts
+++ libs/cmdb-instances/src/data-providers/fetchCmdbInstanceDetail.spec.ts
@@ -1,13 +1,13 @@
 import { fetchCmdbInstanceDetail } from "./fetchCmdbInstanceDetail";
-import { InstanceApi } from "@next-sdk/cmdb-sdk";
+import { InstanceApi_getDetail } from "@next-sdk/cmdb-sdk";
 jest.mock("@next-sdk/cmdb-sdk");
 import { mockFetchCmdbInstanceDetailReturnValue } from "../__mocks__/fetchCmdbInstanceDetail";
 
 describe("fetchCmdbInstanceDetail", () => {
   it("should work", async () => {
-    jest
-      .spyOn(InstanceApi, "getDetail")
-      .mockResolvedValue(mockFetchCmdbInstanceDetailReturnValue);
+    (InstanceApi_getDetail as jest.Mock).mockResolvedValue(
+      mockFetchCmdbInstanceDetailReturnValue
+    );
     const result = await fetchCmdbInstanceDetail("HOST", "58312af20a20d");
     expect(result).toEqual(mockFetchCmdbInstanceDetailReturnValue);
   });
