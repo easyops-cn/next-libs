027280a95b570846528c10caf17298af5d99ee00
ruckerliu <ruckerliu@ruckerliudeMacBook-Air.local>
2021-06-22 18:56:46 +0800

    chore(): size limit

--- .size-limit.js
+++ .size-limit.js
@@ -6,7 +6,7 @@ module.exports = sizeLimit({
     forms: "12 KB",
     "basic-components": "31 KB",
     charts: "20 KB",
-    "cmdb-instances": "80 KB",
+    "cmdb-instances": "110 KB",
     crontab: "25 KB",
     "permission-utils": "18 KB",
     "storyboard-visualization": "45 KB",
