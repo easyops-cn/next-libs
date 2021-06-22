6b82c232a25334abb8099f3e3d9e03194e56fc17
ruckerliu <ruckerliu@ruckerliudeMacBook-Air.local>
2021-06-22 15:51:38 +0800

    fix(): 详情table逻辑调整 Closed INSTANCE-1802

--- libs/cmdb-instances/src/instance-detail/components/instance-relation-table-show/instance-relation-table-show.tsx
+++ libs/cmdb-instances/src/instance-detail/components/instance-relation-table-show/instance-relation-table-show.tsx
@@ -5,6 +5,7 @@ import {
   modifyModelData,
 } from "@next-libs/cmdb-utils";
 import { CmdbModels } from "@next-sdk/cmdb-sdk";
+import { InstanceListTable } from "../../../instance-list-table";
 
 export interface InstanceRelationTableShowProps {
   modelDataMap: { [objectId: string]: CmdbModels.ModelCmdbObject };
@@ -22,20 +23,21 @@ export function InstanceRelationTableShow(
   const oppositeModelData = modifyModelData(
     modelDataMap[relationData.right_object_id]
   );
-
-  const columns = oppositeModelData.attrList.map((r) => ({
-    title: r.name,
-    dataIndex: r.id,
-    key: r.id,
-  }));
   return (
     <div style={{ maxWidth: "500px" }}>
-      <Table
-        columns={columns}
-        scroll={{ x: "max-content" }}
-        dataSource={value}
-        pagination={false}
-      ></Table>
+      <InstanceListTable
+        idObjectMap={props.modelDataMap}
+        modelData={oppositeModelData}
+        instanceListData={{
+          list: value,
+        }}
+        fieldIds={oppositeModelData.attrList.map((attr) => attr.id)}
+        selectDisabled={true}
+        sortDisabled={true}
+        configProps={{
+          pagination: false,
+        }}
+      ></InstanceListTable>
     </div>
   );
 }
