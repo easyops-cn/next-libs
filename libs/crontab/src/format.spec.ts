205cd2e05d2ba7eb1f03c907da7e9ef0c0dd44d2
dophijing <dophijing@163.com>
2021-08-06 11:33:18 +0800

    chore(crontab): add i18n

--- libs/crontab/src/format.spec.ts
+++ libs/crontab/src/format.spec.ts
@@ -1,17 +1,34 @@
 import formatCrontabObject from "./format";
+import i18n from "i18next";
+
+jest.mock("i18next");
 
 describe("crontab format", () => {
-  it("should format correctly", () => {
+  it("should format zh-CN correctly", () => {
+    i18n.language = "zh-CN";
     const crontab = {
       minute: "2",
       hour: "6",
       date: "*",
       month: "*",
-      dow: "*"
+      dow: "*",
     };
 
     const result = formatCrontabObject(crontab);
-
     expect(result).toEqual("在 06:02 AM");
+  });
+
+  it("should format en correctly", () => {
+    i18n.language = "en";
+    const crontab = {
+      minute: "2",
+      hour: "6",
+      date: "*",
+      month: "*",
+      dow: "*",
+    };
+
+    const result = formatCrontabObject(crontab);
+    expect(result).toEqual("At 06:02 AM");
   });
 });
