0ba310171060886ca157a1bf48fc33605adfc7f3
abert <1248824453@qq.com>
2022-01-26 11:32:35 +0800

    feat(): adapt to dark theme and add dark svgs

--- libs/basic-components/src/EmptyResult.tsx
+++ libs/basic-components/src/EmptyResult.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useCurrentTheme } from "@next-core/brick-kit";
+
 import { ReactComponent as BrowserTooOldSvg } from "./empty-result/browser-too-old.svg";
 import { ReactComponent as NoDataSvg } from "./empty-result/no-data.svg";
 import { ReactComponent as NoHistoryVersionSvg } from "./empty-result/no-history-version.svg";
 import { ReactComponent as NoVisitRecordSvg } from "./empty-result/no-visit-record.svg";
 import { ReactComponent as SearchEmptySvg } from "./empty-result/search-empty.svg";
 import { ReactComponent as WelcomeToCreateSvg } from "./empty-result/welcome-to-create.svg";
+
+import { ReactComponent as BrowserTooOldDarkSvg } from "./empty-result/browser-too-old-dark.svg";
+import { ReactComponent as NoDataDarkSvg } from "./empty-result/no-data-dark.svg";
+import { ReactComponent as NoHistoryVersionDarkSvg } from "./empty-result/no-history-version-dark.svg";
+import { ReactComponent as NoVisitRecordDarkSvg } from "./empty-result/no-visit-record-dark.svg";
+import { ReactComponent as SearchEmptyDarkSvg } from "./empty-result/search-empty-dark.svg";
+import { ReactComponent as WelcomeToCreateDarkSvg } from "./empty-result/welcome-to-create-dark.svg";
 
 export enum EmptyResultStatus {
   BrowserTooOld = "browser-too-old",
@@ -30,6 +39,19 @@ const map: { [key in EmptyResultStatus]: React.ReactElement } = {
   [EmptyResultStatus.WelcomeToCreate]: <WelcomeToCreateSvg />,
 };
 
+// 暗色主题图标
+const darkMap: { [key in EmptyResultStatus]: React.ReactElement } = {
+  [EmptyResultStatus.BrowserTooOld]: <BrowserTooOldDarkSvg />,
+  [EmptyResultStatus.NoData]: <NoDataDarkSvg />,
+  [EmptyResultStatus.Empty]: <NoDataDarkSvg />,
+  [EmptyResultStatus.NoHistoryVersion]: <NoHistoryVersionDarkSvg />,
+  [EmptyResultStatus.NoVisitRecord]: <NoVisitRecordDarkSvg />,
+  [EmptyResultStatus.SearchEmpty]: <SearchEmptyDarkSvg />,
+  [EmptyResultStatus.WelcomeToCreate]: <WelcomeToCreateDarkSvg />,
+};
+
 export function EmptyResult(props: EmptyResultProps): React.ReactElement {
-  return map[props.status];
+  const theme = useCurrentTheme();
+  const isDark = theme === "dark-v2";
+  return isDark ? darkMap[props.status] : map[props.status];
 }
