19e81d90823904ef2d1189c6c230d588363f17b3
abert <1248824453@qq.com>
2022-05-31 15:17:37 +0800

    fix(): 新增动态筛选的构件 refs# MONITOR-3554

--- libs/cmdb-instances/src/instance-list-table/AdvancedSearchCondition.spec.tsx
+++ libs/cmdb-instances/src/instance-list-table/AdvancedSearchCondition.spec.tsx
@@ -1,14 +1,17 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import { cleanup, render, fireEvent } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
-import { AdvancedSearchCondition } from "./AdvancedSearchCondition";
-import { AdvancedSearch, AdvancedSearchFormProps } from "./AdvancedSearch";
+import {
+  AdvancedSearchCondition,
+  formatAqToShow,
+} from "./AdvancedSearchCondition";
+import { DynamicSearch, DynamicSearchFormProps } from "./DynamicSearch";
 import { mount } from "enzyme";
 
-jest.mock("./AdvancedSearch", () => ({
-  AdvancedSearch: jest.fn(() => {
-    return "<div>Fake advanced search loaded!</div>";
+jest.mock("./DynamicSearch", () => ({
+  DynamicSearch: jest.fn(() => {
+    return "<div>Fake dynamic search loaded!</div>";
   }),
   getFieldConditionsAndValues: jest.fn(() => {
     return {
@@ -117,338 +120,97 @@ jest.mock("./AdvancedSearch", () => ({
     Exists: "$exists",
   },
 }));
-const HOST: any = {
-  objectId: "HOST",
-  view: {
-    attr_order: [
-      "ip",
-      "_agentStatus",
-      "cpu",
-      "cpuHz",
-      "cpuModel",
-      "cpus",
-      "diskSize",
-    ],
-  },
-  attrList: [
-    {
-      id: "__pipeline",
-      name: "流水线信息",
-      protected: true,
-      value: {
-        type: "str",
-        regex: null,
-        default_type: "",
-        default: null,
-        struct_define: [],
-        mode: "",
-        prefix: "",
-        start_value: 0,
-        series_number_length: 0,
-      },
-    },
-    {
-      id: "hostname",
-      name: "主机名",
-      protected: true,
-      custom: "false",
-      unique: "false",
-      readonly: "false",
-      required: "true",
-      tag: ["默认属性"],
-      description: "",
-      tips: "",
-      value: {
-        type: "str",
-        regex: null,
-        default_type: "value",
-        default: null,
-        struct_define: [],
-        mode: "default",
-        prefix: "",
-        start_value: 0,
-        series_number_length: 0,
-      },
-      wordIndexDenied: false,
-      isInherit: false,
-      notifyDenied: false,
-    },
-    {
-      id: "ip",
-      name: "IP",
-      protected: true,
-      custom: "false",
-      unique: "false",
-      readonly: "false",
-      required: "true",
-      tag: ["默认属性"],
-      description: "",
-      tips: "",
-      value: {
-        type: "ip",
-        regex:
-          "((^\\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\\s*$)|(^\\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:)))(%.+)?\\s*$))",
-        default_type: "",
-        default: null,
-        struct_define: [],
-        mode: "",
-        prefix: "",
-        start_value: 0,
-        series_number_length: 0,
-      },
-      wordIndexDenied: true,
-      isInherit: false,
-      notifyDenied: false,
-    },
-    {
-      id: "cpu",
-      name: "CPU信息",
-      protected: true,
-      custom: "true",
-      unique: "false",
-      readonly: "false",
-      required: "false",
-      tag: [],
-      description: "",
-      tips: "",
-      value: {
-        type: "struct",
-        regex: null,
-        default_type: "",
-        default: null,
-        struct_define: [
-          {
-            id: "brand",
-            name: "型号",
-            type: "str",
-            regex: null,
-            protected: true,
-          },
-          {
-            id: "architecture",
-            name: "架构",
-            type: "str",
-            regex: null,
-            protected: true,
-          },
-          {
-            id: "hz",
-            name: "频率",
-            type: "str",
-            regex: null,
-            protected: true,
-          },
-          {
-            id: "logical_cores",
-            name: "逻辑核数",
-            type: "int",
-            regex: null,
-            protected: true,
-          },
-          {
-            id: "physical_cores",
-            name: "物理核数",
-            type: "int",
-            regex: null,
-            protected: true,
-          },
-        ],
-        mode: "",
-        prefix: "",
-        start_value: 0,
-        series_number_length: 0,
-      },
-      wordIndexDenied: false,
-    },
-    {
-      id: "testSort",
-      name: "流水号",
-      protected: true,
-      custom: "false",
-      unique: "false",
-      readonly: "false",
-      required: "true",
-      tag: ["默认属性"],
-      description: "",
-      tips: "",
-      value: {
-        type: "str",
-        regex: null,
-        default_type: "series-number",
-        default: null,
-        struct_define: [],
-        mode: "default",
-        prefix: "aa",
-        start_value: 0,
-        series_number_length: 2,
-      },
-      wordIndexDenied: false,
-      isInherit: false,
-      notifyDenied: false,
-    },
-    {
-      id: "bool",
-      name: "布尔值",
-      protected: true,
-      custom: "false",
-      unique: "false",
-      readonly: "false",
-      required: "true",
-      tag: ["默认属性"],
-      description: "",
-      tips: "",
-      value: {
-        type: "bool",
-        regex: null,
-        default_type: "",
-        default: null,
-        struct_define: [],
-        mode: "default",
-        prefix: "aa",
-        start_value: 0,
-        series_number_length: "",
-      },
-      wordIndexDenied: false,
-      isInherit: false,
-      notifyDenied: false,
-    },
-  ],
-  relation_list: [
-    {
-      left_description: "负责备份的主机",
-      left_groups: ["_user", "basic_info"],
-      left_id: "backupowner",
-      left_max: -1,
-      left_min: 0,
-      left_name: "备份负责人",
-      left_object_id: "HOST",
-      left_tags: [],
-      name: "",
-      protected: false,
-      relation_id: "HOST_backupowner_USER",
-      right_description: "备份负责人",
-      right_groups: [],
-      right_id: "_backupowner_HOST",
-      right_max: -1,
-      right_min: 0,
-      right_name: "负责备份的主机",
-      right_object_id: "USER",
-      right_tags: [],
-      _version: 0,
-    },
-  ],
-};
 
-const mockAdvancedSearch = AdvancedSearch as any as jest.Mock;
-const mockAdvancedSearchContent = mockAdvancedSearch();
+const Fields: Record<string, any>[] = [
+  {
+    id: "clusterInstance.deployHost.hostname",
+    name: "集群主机名",
+  },
+  {
+    id: "hostname",
+    name: "主机名2",
+  },
+  {
+    id: "ARTIFACT_INSTANCE_HISTORY.HOST.hostname",
+    name: "HostName",
+  },
+];
+
+const mockDynamicSearch = DynamicSearch as any as jest.Mock;
+const mockDynamicSearchContent = mockDynamicSearch();
 
 afterEach(cleanup);
 
 describe("AdvancedSearchCondition", () => {
   it("should call searchCmdbInstances show search conditions", async () => {
-    const objectId = "HOST";
-    const modelData = HOST;
-    const idObjectMap = { [objectId]: modelData };
     const onAdvancedSearch = jest.fn();
-    const fieldIds: string[] = ["hostname", "ip", "_deviceList_CLUSTER"];
-
     const { findByText, queryByText } = render(
       <AdvancedSearchCondition
-        objectId={objectId}
-        objectList={[HOST]}
+        fields={Fields}
         onAdvancedSearch={onAdvancedSearch}
-        fieldIds={fieldIds}
       />
     );
 
     await act(async () => {
       await (global as any).flushPromises();
     });
-    findByText(mockAdvancedSearchContent);
-    const fields: Record<string, boolean> = {};
-    const newFieldIds = ["hostname", "ip", "_deviceList_CLUSTER"];
-    newFieldIds.forEach((id) => (fields[id] = true));
+    findByText(mockDynamicSearchContent);
 
-    expect(mockAdvancedSearch).toBeCalled();
-    const advancedSearchFormProps: AdvancedSearchFormProps =
-      mockAdvancedSearch.mock.calls[
-        mockAdvancedSearch.mock.calls.length - 1
-      ][0];
-    expect(advancedSearchFormProps.idObjectMap).toEqual(idObjectMap);
-    expect(advancedSearchFormProps.modelData).toBe(modelData);
-    expect(advancedSearchFormProps.fieldIds).toEqual(newFieldIds);
-    const mockAdvancedSearchElement = queryByText(mockAdvancedSearchContent);
+    expect(mockDynamicSearch).toBeCalled();
+    const dynamicSearchFormProps: DynamicSearchFormProps =
+      mockDynamicSearch.mock.calls[mockDynamicSearch.mock.calls.length - 1][0];
+    expect(dynamicSearchFormProps.fields).toEqual(Fields);
+    const mockAdvancedSearchElement = queryByText(mockDynamicSearchContent);
     expect(mockAdvancedSearchElement).toBeVisible();
   });
 
-  it("should work when model's isAbstract is true", async () => {
-    const objectId = "HOST";
-    const modelData = { ...HOST, isAbstract: true };
-    const fieldIds: string[] = ["hostname", "ip", "_deviceList_CLUSTER"];
-
-    const wrapper = mount(
-      <AdvancedSearchCondition
-        objectId={objectId}
-        objectList={[modelData]}
-        fieldIds={fieldIds}
-      />
-    );
-
-    await act(async () => {
-      await (global as any).flushPromises();
-    });
-
-    const fields: Record<string, boolean> = {};
-    const newFieldIds = ["hostname", "ip", "_deviceList_CLUSTER", "_object_id"];
-    newFieldIds.forEach((id) => (fields[id] = true));
-
-    const advancedSearchFormProps: AdvancedSearchFormProps =
-      mockAdvancedSearch.mock.calls[
-        mockAdvancedSearch.mock.calls.length - 1
-      ][0];
-    expect(advancedSearchFormProps.fieldIds.length).toBe(4);
-    expect(advancedSearchFormProps.fieldIds).toEqual(newFieldIds);
-  });
-
-  it("should work when fieldIds is empty array", async () => {
-    const objectId = "HOST";
-    const onAdvancedSearch = jest.fn();
-    const { findByText } = render(
-      <AdvancedSearchCondition
-        objectId={objectId}
-        objectList={[HOST]}
-        onAdvancedSearch={onAdvancedSearch}
-      />
-    );
-    await act(async () => {
-      await (global as any).flushPromises();
-    });
-    findByText(mockAdvancedSearchContent);
-    const fields: Record<string, boolean> = {};
-    const newFieldIds = [
-      "ip",
-      "cpu",
-      "__pipeline",
-      "hostname",
-      "testSort",
-      "bool",
+  it("check initAq should pass", async () => {
+    const testdata: Record<string, any>[] = [
+      {
+        aq: [
+          {
+            $or: [{ hostname: { $like: "%aaa%" } }],
+          },
+        ],
+        expected: [
+          {
+            $or: [{ hostname: { $like: "%aaa%" } }],
+          },
+        ],
+      },
+      {
+        aq: [
+          {
+            $or: [
+              {
+                "ARTIFACT_INSTANCE_HISTORY.HOST.hostname": {
+                  $like: "%192.168.100.162%",
+                },
+              },
+            ],
+          },
+        ],
+        expected: [
+          {
+            $or: [
+              {
+                "ARTIFACT_INSTANCE_HISTORY.HOST.hostname": {
+                  $like: "%192.168.100.162%",
+                },
+              },
+            ],
+          },
+        ],
+      },
     ];
-    expect(mockAdvancedSearch).toBeCalled();
-    const advancedSearchFormProps: AdvancedSearchFormProps =
-      mockAdvancedSearch.mock.calls[
-        mockAdvancedSearch.mock.calls.length - 1
-      ][0];
-    newFieldIds.forEach((id) => (fields[id] = true));
-    expect(advancedSearchFormProps.fieldIds).toEqual(newFieldIds);
+    testdata.forEach((t) => {
+      expect(formatAqToShow(t.aq)).toEqual(t.expected);
+    });
   });
 
   it("should work with hideSearchConditions", async () => {
     const wrapper = mount(
-      <AdvancedSearchCondition
-        objectId="HOST"
-        objectList={[HOST]}
-        hideSearchConditions={true}
-      />
+      <AdvancedSearchCondition fields={Fields} hideSearchConditions={true} />
     );
     await (global as any).flushPromises();
     await jest.runAllTimers();
@@ -457,23 +219,9 @@ describe("AdvancedSearchCondition", () => {
   });
 
   it("should work with hideSearchConditions", async () => {
-    const _HOST = {
-      ...HOST,
-      relation_list: [
-        ...HOST.relation_list,
-        {
-          left_id: "HOST_A",
-          left_object_id: "HOST",
-          relation_id: "HOST_SELF",
-          right_id: "HOST_B",
-          right_object_id: "HOST",
-        },
-      ],
-    };
     const wrapper = mount(
       <AdvancedSearchCondition
-        objectId="HOST"
-        objectList={[_HOST]}
+        fields={Fields}
         aq={[
           {
             $or: [
@@ -484,31 +232,12 @@ describe("AdvancedSearchCondition", () => {
               },
             ],
           },
-          {
-            ip: {
-              $exists: true,
-            },
-          },
-          {
-            $or: [
-              {
-                cpu: {
-                  $eq: "closed",
-                },
-              },
-            ],
-          },
-          {
-            "backupowner.name": {
-              $exists: false,
-            },
-          },
         ]}
       />
     );
     await (global as any).flushPromises();
     await jest.runAllTimers();
     wrapper.update();
-    expect(wrapper.find("Tag").length).toBe(4);
+    expect(wrapper.find("Tag").length).toBe(1);
   });
 });
---
+++ libs/cmdb-instances/src/instance-list-table/DynamicSearch.tsx
@@ -0,0 +1,302 @@
+import React, { FormEvent } from "react";
+import update from "immutability-helper";
+import { isEqual, isNil, isString, isArray, isEmpty } from "lodash";
+import { Form } from "@ant-design/compatible";
+import { Button, Col, Input, Row, Select } from "antd";
+import { FormComponentProps } from "@ant-design/compatible/lib/form";
+import {
+  ComparisonOperators,
+  LogicalOperators,
+  Query,
+  QueryOperatorExpressions,
+} from "@next-libs/cmdb-utils";
+import styles from "./DynamicSearch.module.css";
+import i18n from "i18next";
+import { NS_LIBS_CMDB_INSTANCES, K } from "../i18n/constants";
+import {
+  processAttrValueWithQuote,
+  ENABLED_CMDB_ADVANCE_SEARCH_WITH_QUOTE,
+} from "../processors";
+
+export interface Field {
+  id: string;
+  name: string;
+  values?: any[];
+}
+
+export interface DynamicSearchFormProps extends FormComponentProps {
+  fields: Record<string, any>[];
+  q?: Query[];
+  onSearch?(
+    queries: Query[],
+    queriesToShow?: Query[],
+    fieldToShow?: Record<string, any[]>[]
+  ): void;
+  fieldToShow?: Record<string, any[]>[]; // 回填高级搜索的field值（根据query回填丢失了引号信息）
+}
+
+export interface DynamicSearchFormState {
+  fields: Field[];
+}
+
+export class DynamicSearchForm extends React.Component<
+  DynamicSearchFormProps,
+  DynamicSearchFormState
+> {
+  constructor(props: DynamicSearchFormProps) {
+    super(props);
+
+    this.state = this.computeState();
+  }
+
+  componentDidUpdate(prevProps: Readonly<DynamicSearchFormProps>): void {
+    if (!isEqual(this.props.q, prevProps.q)) {
+      const newState = this.computeState();
+      this.setState(newState);
+      this.setFormFieldValues(newState.fields);
+    }
+  }
+
+  computeState(): { fields: Field[] } {
+    const fieldQueryOperatorExpressionsMap: Record<
+      string,
+      QueryOperatorExpressions
+    > = {};
+    let fields: Field[] = [];
+    if (this.props.q) {
+      this.props.q.forEach((query) => {
+        Object.entries(query).forEach(([key, expressions]) => {
+          if (key === LogicalOperators.Or || key === LogicalOperators.And) {
+            const firstSubQuery = (expressions as Query[])[0];
+            const fieldId = Object.keys(firstSubQuery)[0];
+            const compareOperator = Object.keys(firstSubQuery[fieldId])[0];
+            let subQueryValue = "";
+            let targetField;
+            if (ENABLED_CMDB_ADVANCE_SEARCH_WITH_QUOTE) {
+              targetField = this.props.fieldToShow?.find(
+                (fieldItem) => Object.keys(fieldItem)[0] === fieldId
+              );
+            }
+            if (!isNil(targetField)) {
+              // 二维数组会有问题，
+              subQueryValue = isArray(targetField[fieldId])
+                ? targetField[fieldId]
+                    .map((item) => (isArray(item) ? item.join(" ") : item))
+                    .join("")
+                : (targetField[fieldId] as any);
+            } else {
+              subQueryValue = (expressions as Query[])
+                .map((query) => {
+                  const targetValue = (
+                    query[fieldId] as Record<ComparisonOperators, any>
+                  )[compareOperator as ComparisonOperators];
+                  if (
+                    compareOperator === ComparisonOperators.Like ||
+                    compareOperator === ComparisonOperators.NotLike
+                  ) {
+                    return isString(targetValue) && targetValue.includes(" ")
+                      ? `${targetValue[0]}"${targetValue.slice(
+                          1,
+                          targetValue.length - 1
+                        )}"${targetValue[targetValue.length - 1]}`
+                      : targetValue;
+                  } else {
+                    return isString(targetValue) && targetValue.includes(" ")
+                      ? `"${targetValue}"`
+                      : targetValue;
+                  }
+                })
+                .join(" ");
+            }
+            key = fieldId;
+            expressions = {
+              [compareOperator]: subQueryValue,
+            };
+          }
+
+          fieldQueryOperatorExpressionsMap[key] =
+            expressions as QueryOperatorExpressions;
+        });
+      });
+    }
+    if (this.props.fields) {
+      fields = this.props.fields.map(
+        (v) => ({ ...v, values: [null] } as Field)
+      );
+      if (!isEmpty(fieldQueryOperatorExpressionsMap)) {
+        fields = this.props.fields.map((v) => {
+          let str: any = null;
+          if (fieldQueryOperatorExpressionsMap[v.id]) {
+            const expressions = fieldQueryOperatorExpressionsMap[v.id]["$like"];
+            str = expressions.replace(/%/g, "");
+          }
+          return {
+            ...v,
+            values: [str],
+          } as Field;
+        });
+      }
+    }
+    return { fields };
+  }
+
+  setFormFieldValues(fields: Field[]): void {
+    const values = fields.reduce<{ [key: string]: any }>((acc, field) => {
+      acc[`${field.id}`] = field.values.join("");
+      return acc;
+    }, {});
+    this.props.form.setFieldsValue(values);
+  }
+
+  onValueChange = (value: any, fieldIndex: number, valueIndex: number) => {
+    this.setState({
+      fields: update(this.state.fields, {
+        [fieldIndex]: { values: { [valueIndex]: { $set: value } } },
+      }),
+    });
+  };
+
+  getFields() {
+    const { getFieldDecorator } = this.props.form;
+    return this.state.fields.map((field, fieldIndex) => {
+      return (
+        <Col span={12} key={field.id}>
+          <Form.Item label={field.name}>
+            <Input.Group className={styles.conditionInputGroup} compact>
+              <Select dropdownMatchSelectWidth={false} defaultValue={"contain"}>
+                <Select.Option value={"contain"} key={"contain"}>
+                  {i18n.t(
+                    `${NS_LIBS_CMDB_INSTANCES}:${K.OPERATOR_CONTAIN_DEFINE}`
+                  )}
+                </Select.Option>
+              </Select>
+              {getFieldDecorator(`${field.id}`, {
+                initialValue: field.values || "",
+              })(
+                <Input
+                  placeholder={i18n.t(
+                    `${NS_LIBS_CMDB_INSTANCES}:${K.ADVANCE_SEARCH_SINGLE_INPUT_PLACEHOLDER}`
+                  )}
+                  onChange={(e) => this.handleValueChange(e, fieldIndex)}
+                />
+              )}
+            </Input.Group>
+          </Form.Item>
+        </Col>
+      );
+    });
+  }
+
+  handleValueChange = (e: any, fieldIndex: number) => {
+    this.onValueChange(e.target.value, fieldIndex, 0);
+  };
+
+  handleSearch = (e: FormEvent<any>): void => {
+    e.preventDefault();
+    if (this.props.onSearch) {
+      let queries: Query[] = [];
+      let queriesToShow: Query[] = [];
+      const fieldToShow: Record<string, any[]>[] = [];
+      this.state.fields.forEach((field) => {
+        const expressions: QueryOperatorExpressions = {};
+        const fieldId = field.id;
+        let fieldQuery: Query = { [fieldId]: expressions };
+        let fieldQueryToShow: Query = { [fieldId]: expressions };
+        let hasValue = false;
+        const operator = {
+          comparisonOperator: ComparisonOperators.Like,
+          logicalOperator: LogicalOperators.Or,
+        };
+        field.values.forEach((value) => {
+          fieldQuery = { [field.id]: expressions };
+          const fieldIdToShow = field.id;
+          fieldQueryToShow = { [fieldIdToShow]: expressions };
+          if (
+            value !== null &&
+            value !== "" &&
+            !(Array.isArray(value) && value.length === 0)
+          ) {
+            let values: any[];
+            if (typeof value === "string") {
+              values = processAttrValueWithQuote(value, []);
+            }
+            if (values.length) {
+              fieldQuery = {
+                [operator.logicalOperator]: values.map((value) => {
+                  return {
+                    [fieldId]: {
+                      [operator.comparisonOperator]: `%${value}%`,
+                    },
+                  };
+                }),
+              };
+            }
+            fieldQueryToShow = {
+              [operator.logicalOperator]: values.map((value) => {
+                return {
+                  [fieldIdToShow]: {
+                    [operator.comparisonOperator]: `%${value}%`,
+                  },
+                };
+              }),
+            };
+            hasValue = true;
+            fieldToShow.push({ [field.id]: field.values });
+          }
+        });
+        if (hasValue) {
+          queries.push(fieldQuery);
+          queriesToShow.push(fieldQueryToShow);
+        }
+      });
+      if (queries.length === 0) {
+        queries = undefined;
+        queriesToShow = undefined;
+      }
+      if (!isEqual(this.props.q, queries)) {
+        this.props.onSearch(queries, queriesToShow, fieldToShow);
+      }
+    }
+  };
+
+  handleReset = () => {
+    this.props.onSearch([], [], []);
+    this.props.form.resetFields();
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div className={styles.dynamicSearch}>
+        <Form
+          layout="vertical"
+          className={styles.dynamicSearchForm}
+          onSubmit={this.handleSearch}
+        >
+          <Row gutter={50}>{this.getFields()}</Row>
+          <Row>
+            <Col span={24} style={{ textAlign: "right" }}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                data-testid="submit-button"
+              >
+                {i18n.t(`${NS_LIBS_CMDB_INSTANCES}:${K.SEARCH}`)}
+              </Button>
+              <Button
+                style={{ marginLeft: 8 }}
+                onClick={this.handleReset}
+                data-testid="reset-button"
+              >
+                {i18n.t(`${NS_LIBS_CMDB_INSTANCES}:${K.RESET}`)}
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export const DynamicSearch = Form.create<DynamicSearchFormProps>({
+  name: "dynamic_search",
+})(DynamicSearchForm);
