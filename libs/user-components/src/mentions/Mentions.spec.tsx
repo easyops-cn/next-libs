20b9a7e1de41cb0284503cce023173b8f2903180
yingjiechen <1910026791@qq.com>
2022-11-23 19:37:08 +0800

    fix(CommentBrick): 查询结果默认显示showkey  Refs: ITSERVICE_NEXT-5114

--- libs/user-components/src/mentions/Mentions.spec.tsx
+++ libs/user-components/src/mentions/Mentions.spec.tsx
@@ -3,6 +3,28 @@ import { Mentions } from "./Mentions";
 import "@testing-library/jest-dom";
 import { mount, shallow } from "enzyme";
 import { Mentions as AMentions } from "antd";
+
+jest.mock("@next-sdk/cmdb-sdk");
+jest.mock("@next-libs/hooks/src/mention/useMention");
+const data = [
+  {
+    instanceId: "5853285536d5d",
+    name: "easyops1",
+    nickname: "",
+    user_email: "aaa@easyops.cn",
+    user_icon: "assets/favicon.png",
+  },
+  {
+    instanceId: "588e94254e66d",
+    name: "easyops_invalid",
+    user_email: "xxxxx@easyops.cn",
+    user_icon: "assets/favicon.png",
+  },
+];
+jest.mock("@next-libs/hooks", () => ({
+  useMention: () => ({ users: data, updateUserName: jest.fn }),
+}));
+
 describe("Mentions", () => {
   beforeAll(() => {
     jest.useFakeTimers();
@@ -17,14 +39,19 @@ describe("Mentions", () => {
     const wrapper = shallow(<Mentions onSearch={mockOnSearch} />);
     wrapper.find(AMentions).invoke("onSearch")("a", "b");
     expect(mockOnSearch).toHaveBeenCalledWith("a", "b");
-    expect(wrapper.find(AMentions.Option)).toHaveLength(0);
+    expect(wrapper.find(AMentions.Option)).toHaveLength(2);
+    expect(wrapper.find(AMentions.Option).at(0).prop("value")).toBe("easyops1");
   });
 
-  it("should work showkey", () => {
+  it("should work showkey", async () => {
     const mockOnSearch = jest.fn();
     const wrapper = shallow(
       <Mentions onSearch={mockOnSearch} showkey={["name", "user_email"]} />
     );
-    expect(wrapper.find(AMentions.Option)).toHaveLength(0);
+    await jest.runAllTimers();
+    expect(wrapper.find(AMentions.Option)).toHaveLength(2);
+    expect(wrapper.find(AMentions.Option).at(0).prop("value")).toBe(
+      "easyops1/aaa@easyops.cn"
+    );
   });
 });
