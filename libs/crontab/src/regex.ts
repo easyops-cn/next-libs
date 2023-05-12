97b72c12b4e8ab24c56e314ebceedb643bc6197f
julielai <julielai@easyops.cn>
2023-05-12 17:37:53 +0800

    fix(): crontab支持L字符

--- libs/crontab/src/regex.ts
+++ libs/crontab/src/regex.ts
@@ -1,14 +1,16 @@
 const getCrontabRegex = function (
   replacement: string,
-  numbers?: string
+  numbers?: string,
+  isDate?: boolean
 ): RegExp {
   const splitRegexTemplate = "^(%s(,%s)*)$";
   // numbers 用于 月份和星期的数字部分
   const elementRegexTemplate = "\\*(\\/(?!0)%n)?|%s-%s(\\/(?!0)%n)?|%s";
 
-  const elementRegex = elementRegexTemplate
+  let elementRegex = elementRegexTemplate
     .replace(/%n/g, `(${numbers || replacement})`)
     .replace(/%s/g, `(${replacement})`);
+  isDate ? (elementRegex = elementRegex + "|L") : "";
 
   return new RegExp(
     splitRegexTemplate.replace(/%s/g, `(${elementRegex})`),
@@ -19,7 +21,7 @@ const getCrontabRegex = function (
 export default {
   minute: getCrontabRegex("[0-5]?\\d"),
   hour: getCrontabRegex("[01]?\\d|2[0-3]"),
-  date: getCrontabRegex("L|0?[1-9]|[12]\\d|3[01]"),
+  date: getCrontabRegex("0?[1-9]|[12]\\d|3[01]", null, true),
   month: getCrontabRegex(
     "[1-9]|1[012]|[jJ]an|[fF]eb|[mM]ar|[aA]pr|[mM]ay|[jJ]un|[jJ]ul|[aA]ug|[sS]ep|[oO]ct|[nN]ov|[dD]ec",
     "[1-9]|1[012]"
