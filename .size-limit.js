fa235145c4c0cc00363ec6c63e7294019b29df45
abert <abertluo@easyops.cn>
2023-08-07 15:42:08 +0800

    fix(): 修复子模型没有过滤实例的功能 refs: CMDB_INSTANCE-345

--- .size-limit.js
+++ .size-limit.js
@@ -3,7 +3,7 @@ const { sizeLimit } = require("@next-core/build-config-factory");
 module.exports = sizeLimit({
   libs: {
     "*": "20 KB",
-    "cmdb-instances": "96 KB",
+    "cmdb-instances": "97 KB",
     "storyboard-visualization": "40 KB",
   },
 });
