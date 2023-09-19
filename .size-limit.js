9f8083498ccc23edf36f8e3030a3f097321b7573
annzhang <annzhang@easyops.cn>
2023-09-19 15:40:07 +0800

    fix(cmdb): 越权查询时更多关系实例也要用越权接口 Refs CMDB_INSTANCE-640

--- .size-limit.js
+++ .size-limit.js
@@ -3,7 +3,7 @@ const { sizeLimit } = require("@next-core/build-config-factory");
 module.exports = sizeLimit({
   libs: {
     "*": "20 KB",
-    "cmdb-instances": "97.5 KB",
+    "cmdb-instances": "97.7 KB",
     "storyboard-visualization": "40 KB",
   },
 });
