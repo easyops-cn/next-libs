5ff80d56275748a78775990b31c8165e16e98ed3
zhouj <zhouj@zhoujdeMacBook-Air.local>
2023-12-12 17:03:26 +0800

    fix(): 资源变更历史，变更类型 列表页与详情页要保持一致
    Closes CMDB_CONSUME-124

--- libs/cmdb-utils/src/i18n/locales/zh.ts
+++ libs/cmdb-utils/src/i18n/locales/zh.ts
@@ -1,13 +1,13 @@
 import { Locale, K } from "../constants";
 
 const locale: Locale = {
-  [K.CREATE]: "新建",
+  [K.CREATE]: "新增",
   [K.EDIT]: "编辑",
   [K.DELETE]: "删除",
   [K.ACTIVE]: "激活",
   [K.ARCHIVE]: "归档",
   [K.AUTOMATIC_DISCOVERY]: "自动发现",
-  [K.CREATE_IN_BATCHES]: "批量新建",
+  [K.CREATE_IN_BATCHES]: "批量新增",
   [K.EDIT_IN_BATCHES]: "批量编辑",
   [K.INSTANCE]: "实例",
   [K.INSTANCE_RELATION]: "实例关系",
