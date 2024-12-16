b72baa56b08a226766d307b0ba1dc405188676d3
李佳明 <jimmyli@easyops.cn>
2024-12-16 17:44:28 +0800

    feat(): FormItemWrapper 的 labelTooltip 支持换行 Closes SHLSTYYWPT-740

--- libs/forms/src/FormItemWrapper.spec.tsx
+++ libs/forms/src/FormItemWrapper.spec.tsx
@@ -403,6 +403,23 @@ describe("FormItemWrapper", () => {
     expect(wrapper.find(".labelTooltipIcon").length).toBe(1);
   });
 
+  it("should work when the type of labelTooltip is string with multiple lines", () => {
+    const wrapper = mount(
+      <FormItemWrapper
+        name="username"
+        label="hello"
+        labelTooltip={`line1
+line2`}
+      />
+    );
+    const title = wrapper.find(Tooltip).at(0).prop("title");
+    expect(title.length).toBe(2);
+    expect(title[0].type).toBe("div");
+    expect(title[0].props.children).toBe("line1");
+    expect(title[1].type).toBe("div");
+    expect(title[1].props.children).toBe("line2");
+  });
+
   it("should work when the type of labelTooltip is LabelTooltipProps", () => {
     const icon: any = {
       lib: "antd",
