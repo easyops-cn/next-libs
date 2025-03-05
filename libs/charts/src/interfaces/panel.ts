89f41f0c32ad68b00b334931bc3fec6f12fd60ce
weareoutman <wangshenwei@qq.com>
2025-03-05 16:52:46 +0800

    fix(formatValue): allow disable fixed precision

--- libs/charts/src/interfaces/panel.ts
+++ libs/charts/src/interfaces/panel.ts
@@ -13,4 +13,10 @@ export interface Format {
   type?: FormatType;
   precision?: number;
   unit?: string;
+  /**
+   * 是否固定小数点位数。设置为 `fixedPrecision: false` 时，`1.50` 会变成 `1.5`。
+   *
+   * @default true
+   */
+  fixedPrecision?: boolean;
 }
--- libs/charts/src/utils/number.spec.ts
+++ libs/charts/src/utils/number.spec.ts
@@ -7,6 +7,9 @@ describe("dateRate", () => {
     expect(humanizeNumberValue(8000)).toEqual("8.00K");
     expect(humanizeNumberValue(8000000)).toEqual("8.00M");
     expect(humanizeNumberValue(8000000000)).toEqual("8.00B");
+    expect(
+      humanizeNumberValue(8000000000, undefined, undefined, false)
+    ).toEqual("8B");
     expect(humanizeNumberValue(8000000000000)).toEqual("8.00T");
     expect(humanizeNumberValue(8000, ShortUnitId.K)).toEqual("8.00M");
     expect(humanizeNumberValue(8000, ShortUnitId.M, 1)).toEqual("8.0B");
@@ -18,5 +21,6 @@ describe("dateRate", () => {
     expect(humanizeNumberValue(-8000000000000)).toEqual("-8.00T");
     expect(humanizeNumberValue(-8000, ShortUnitId.K)).toEqual("-8.00M");
     expect(humanizeNumberValue(-8000, ShortUnitId.M, 1)).toEqual("-8.0B");
+    expect(humanizeNumberValue(-8000, ShortUnitId.M, 1, false)).toEqual("-8B");
   });
 });
